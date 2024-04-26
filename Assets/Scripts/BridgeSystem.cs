0d499d2ede848949c3327dfdcf6f4503d51426b4
coniya <cbu010@email.latech.edu>
2024-04-25 20:02:23 -0500

    2d planks

--- Assets/Scripts/BridgeSystem.cs
+++ Assets/Scripts/BridgeSystem.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class BridgeSystem : MonoBehaviour
 {
-    public Transform targetPosition; //position the plank will land on the bridge
+    public Vector2 targetPosition; //position the plank will land on the bridge
 
     private bool hasCollided = false;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !hasCollided)
         {
@@ -20,6 +20,6 @@ public class BridgeSystem : MonoBehaviour
 
     private void MoveToTargetPosition()
     {
-        transform.position = targetPosition.position;
+        transform.position = targetPosition;
     }
 }
