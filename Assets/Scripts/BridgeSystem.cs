f053065641df31e66220bdce938d4afac806da51
coniya <cbu010@email.latech.edu>
2024-04-30 13:19:52 -0500

    bridge system complete

--- Assets/Scripts/BridgeSystem.cs
+++ Assets/Scripts/BridgeSystem.cs
@@ -1,25 +1,63 @@
+using NavMeshPlus.Components;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Tilemaps;
 
 public class BridgeSystem : MonoBehaviour
 {
-    public Vector2 targetPosition; //position the plank will land on the bridge
+    public Vector3Int targetTilePosition; //position the plank will land on the bridge
+    public Tilemap groundTilemap;
+    public Tilemap groundOverlayTilemap;
+    public float rotationAngle; // The angle by which you want to rotate the item
 
     private bool hasCollided = false;
+    private NavMeshSurface navMeshSurface; //reference to the NavMeshSurface component
+    private Rigidbody2D plankRigidbody; // Reference to the Rigidbody2D component
+
+    private void Start()
+    {
+        // Get the Rigidbody2D component attached to this GameObject
+        plankRigidbody = GetComponent<Rigidbody2D>();
+
+        //find navMeshSurface in scene
+        navMeshSurface = FindObjectOfType<NavMeshSurface>();
+    }
+    private void Update()
+    {
+        // Check if the "G" key is pressed and the collision flag is set
+        if (Input.GetKeyDown(KeyCode.G) && hasCollided)
+        {
+            Debug.Log("G is pressed and collided");
+            MoveToTargetTile();
+
+            // Rebuild the NavMesh after moving the item
+            if (navMeshSurface != null)
+            {
+                navMeshSurface.BuildNavMesh();
+            }
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !hasCollided)
         {
-            MoveToTargetPosition();
             hasCollided=true;
+            Debug.Log("has collided");
         }
     }
 
-    private void MoveToTargetPosition()
+    private void MoveToTargetTile()
     {
-        transform.position = targetPosition;
+        // Move the item to the target tile position
+        Vector3 targetWorldPosition = groundTilemap.GetCellCenterWorld(targetTilePosition);
+        transform.position = targetWorldPosition;
+
+        // Rotate the item along z axis
+        transform.eulerAngles = new Vector3(0f, 0f, rotationAngle);
+
     }
 }
