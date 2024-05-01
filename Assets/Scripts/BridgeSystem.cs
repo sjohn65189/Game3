5f7d39f4a42ecbad0eff07e024d3785d759fc0d6
coniya <cbu010@email.latech.edu>
2024-05-01 10:31:47 -0500

    grab sound added to bridge pieces

--- Assets/Scripts/BridgeSystem.cs
+++ Assets/Scripts/BridgeSystem.cs
@@ -1,6 +1,7 @@
 using NavMeshPlus.Components;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.AI;
@@ -12,6 +13,9 @@ public class BridgeSystem : MonoBehaviour
     public Tilemap groundTilemap;
     public Tilemap groundOverlayTilemap;
     public float rotationAngle; // The angle by which you want to rotate the item
+    public AudioClip pickupSound; // Add this field to hold the pickup sound
+    public AudioSource audioSource; // Add this field to reference the AudioSource component
+
 
     private bool hasCollided = false;
     private NavMeshSurface navMeshSurface; //reference to the NavMeshSurface component
@@ -30,7 +34,12 @@ public class BridgeSystem : MonoBehaviour
         // Check if the "G" key is pressed and the collision flag is set
         if (Input.GetKeyDown(KeyCode.G) && hasCollided)
         {
-            
+            // Play the pickup sound
+            if (pickupSound != null && audioSource != null)
+            {
+                audioSource.clip = pickupSound;
+                audioSource.Play();
+            }
             MoveToTargetTile();
 
             // Rebuild the NavMesh after moving the item
