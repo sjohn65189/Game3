cca1ccbfc6b93569ae9ce3848df19a3d5114308d
JJ <jvirg4@gmail.com>
2024-04-30 20:15:20 -0500

    Timer Functionality

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,29 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
-using System.Timers;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    public static System.Timers.Timer timer;
-    void Start() 
-    {
-        StartTimer();
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
 
-    }
-
-    void StartTimer()
+    // Update is called once per frame
+    void Update()
     {
-        // Initialize timer
-        timer = new System.Timers.Timer();
-        timer.Interval = 1000; // 1 second interval
-        
-    }
-
-    void ResetTimer(int clock) 
-    {
-        clock = 0;
+        elapsedTime += Time.deltaTime;
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerText.text = "Time: " + seconds.ToString("D4");
     }
 }
