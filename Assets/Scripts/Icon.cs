52e21674eada0168f9db2ac35837b8afe3877e5b
sjohn65189 <70227912+sjohn65189@users.noreply.github.com>
2024-05-14 12:44:07 -0500

    fixed the icons to where it plays when the sounds were initial off

--- Assets/Scripts/Icon.cs
+++ Assets/Scripts/Icon.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class Icon : MonoBehaviour
 {
+    GameManager gameM;
     public GameObject GameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameM = GameManager.GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -20,14 +21,26 @@ public class Icon : MonoBehaviour
 
     public void MusicButtonClicked() 
     {
-        GameManager gameM = GameManager.GetComponent<GameManager>();
-        gameM.Main_Music.mute = !gameM.Main_Music.mute;
+        if (PlayerPrefs.GetInt("MusicEnabled", 0) == 0)
+        {
+            PlayerPrefs.SetInt("MusicEnabled", 1);
+            gameM.Main_Music.Play();
+        }
+        else {
+            gameM.Main_Music.mute = !gameM.Main_Music.mute;
+        } 
     }
 
     public void GameSoundButtonClicked() 
     {
-        GameManager gameM = GameManager.GetComponent<GameManager>();
-        gameM.Wind_Sound.mute = !gameM.Wind_Sound.mute;
+        if (PlayerPrefs.GetInt("SFXEnabled", 0) == 0)
+        {
+            PlayerPrefs.SetInt("SFXEnabled", 1);
+            gameM.Wind_Sound.Play();
+        }
+        else {
+            gameM.Wind_Sound.mute = !gameM.Wind_Sound.mute;
+        } 
     }
 
 }
