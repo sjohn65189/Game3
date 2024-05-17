83673a3bbd7f9729bcabef5e2a8f6d269ee71e6b
coniya <cbu010@email.latech.edu>
2024-05-16 20:25:42 -0500

    powerups

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,102 +5,159 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-	[HideInInspector] public PlayerInputActions input;
-	
-	private static List<string> collectedItems = new List<string>(); // Static list to keep track of collected items across all instances
+    [HideInInspector] public PlayerInputActions input;
 
-	public string itemName; //Name of Item
-	public Image[] uiSprites; // Array of UI sprites to display collected items
+    private static List<string> collectedItems = new List<string>(); // Static list to keep track of collected items across all instances
 
-	public AudioClip pickupSound; // Add this field to hold the pickup sound
-	public AudioSource audioSource; // Add this field to reference the AudioSource component
+    public string itemName; //Name of Item
+    public Image[] uiSprites; // Array of UI sprites to display collected items
 
-	public GameObject FloatingTextPrefab; // This hold the floating text prefab in unity prefabs folder
+    public AudioClip pickupSound; // Add this field to hold the pickup sound
+    public AudioSource audioSource; // Add this field to reference the AudioSource component
 
+    public GameObject FloatingTextPrefab; // This hold the floating text prefab in unity prefabs folder
 
-    void Start() 
-	{
-		input = new PlayerInputActions();
-		input.Enable();
+    public enum ItemPower
+    {
+        None,
+        PlayerSpeedup,
+        YetiSlowDown,
+    }
 
-		//Reset items list when the game is reset
-		ResetCollectedItems();
-	}
-	
-	private void OnDisable()
-	{
-		input.Disable();
-	}
+    public ItemPower itemPower;
 
-	void OnCollisionStay2D(Collision2D collision)
-	{
-		if (collision.gameObject.CompareTag("Player"))
-		{  
-			if (input.PlayerActions.PickUpItem.IsPressed()) 
-			{
-				CollectItem();
-				// Play the pickup sound
-				if (pickupSound != null && audioSource != null)
-				{
-					audioSource.volume = 0.5f;
-					audioSource.clip = pickupSound;
-					audioSource.Play();
-				}
+    private PlayerController playerController;
+    public Yeti yeti;
 
-				// Add to score for picking up relic
-				ScoreManagerExpanded.instance.GetRelic();
+    private float powerUpDuration = 10f; //duration of powerup
+    public bool isPowerUpActive = false; //flag to track if active
+    private float powerUpTimer = 0f; //timer
+    void Start()
+    {
+        // Find the PlayerController and Yeti components
+        playerController = FindObjectOfType<PlayerController>();
+        if (playerController == null)
+        {
+            Debug.LogError("PlayerController component not found");
+        }
 
-				// Show floating text
-				if (FloatingTextPrefab)
-				{
-					//ShowFloatingText();
-				}
-			}
-		}
-	}
+        input = new PlayerInputActions();
+        input.Enable();
 
-	void CollectItem() {
-		//check if item has already been collected
-		if (!collectedItems.Contains(itemName))
-		{
-			// Add the item to the list of collected items
-			collectedItems.Add(itemName);
+        //Reset items list when the game is reset
+        ResetCollectedItems();
+    }
 
-			// Disable the collected item's renderer and collider
-			gameObject.GetComponent<SpriteRenderer>().enabled = false;
-			gameObject.GetComponent<Collider2D>().enabled = false;
+    private void OnDisable()
+    {
+        input.Disable();
+    }
 
-			// Update the UI to display the collected items
-			UpdateCollectedItemsUI();
-		}
-	}
+    private void Update()
+    {
+        if (isPowerUpActive)
+        {
+            powerUpTimer += Time.deltaTime;
+            if (powerUpTimer >= powerUpDuration)
+            {
+                DeactivatePowerUp();
+            }
+        }
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (input.PlayerActions.PickUpItem.IsPressed())
+            {
+                CollectItem();
+                // Play the pickup sound
+                if (pickupSound != null && audioSource != null)
+                {
+                    audioSource.volume = 0.5f;
+                    audioSource.clip = pickupSound;
+                    audioSource.Play();
+                }
 
-	void UpdateCollectedItemsUI()
-	{
-		// Print the names of collected items to the console for demonstration
-		foreach (var item in collectedItems)
-		{
-			Debug.Log("Collected Item: " + item);
-		}
-		// Loop through all collected items and update UI sprites accordingly
-		for (int i = 0; i < collectedItems.Count && i < uiSprites.Length; i++)
-		{
-			// Assuming your items have a sprite associated with their name
-			Sprite itemSprite = Resources.Load<Sprite>("ArtifactSprites/" + collectedItems[i]); // Adjust the path as needed
+                // Add to score for picking up relic
+                ScoreManagerExpanded.instance.GetRelic();
 
-			// Assign the collected item sprite to the corresponding UI sprite
-			uiSprites[i].sprite = itemSprite;
-		}
-	}
+                // Show floating text
+                if (FloatingTextPrefab)
+                {
+                    //ShowFloatingText();
+                }
+            }
+        }
+    }
 
-	void ResetCollectedItems() 
-	{
-		//clear collected items list when game is restarted
-		collectedItems.Clear();
-	}
+    void CollectItem()
+    {
+        //check if item has already been collected
+        if (!collectedItems.Contains(itemName))
+        {
+            // Add the item to the list of collected items
+            collectedItems.Add(itemName);
 
-	void ShowFloatingText()
-	{
-		Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);	
-	}
+            // Disable the collected item's renderer and collider
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<Collider2D>().enabled = false;
+
+            ActivateItemPower();
+        }
+    }
+
+    void ActivateItemPower()
+    {
+        switch (itemPower)
+        {
+            case ItemPower.PlayerSpeedup:
+                //increase player speed to 7
+                playerController.moveSpeed = 7f;
+                Debug.Log("Increased speed!");
+                StartPowerUpTimer();
+                break;
+            case ItemPower.YetiSlowDown:
+                //slow yeti speed down
+                yeti.AdjustSpeed(3.0f);
+                Debug.Log("Yeti speed decreased!");
+                StartPowerUpTimer();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    void StartPowerUpTimer()
+    {
+        Debug.Log("Player has powerup");
+        isPowerUpActive = true;
+        powerUpTimer = 0f;
+    }
+
+    void DeactivatePowerUp()
+    {
+        Debug.Log("Player no longer has powerup");
+        isPowerUpActive = false;
+        powerUpTimer = 0f;
+        if (itemPower == ItemPower.PlayerSpeedup)
+        {
+            playerController.moveSpeed = 5f;
+        }
+        if (itemPower == ItemPower.YetiSlowDown)
+        {
+            yeti.AdjustSpeed(5.5f);
+        }
+    }
+    void ResetCollectedItems()
+    {
+        //clear collected items list when game is restarted
+        collectedItems.Clear();
+    }
+
+    void ShowFloatingText()
+    {
+        Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+    }
 }
--- Assets/Scripts/Yeti.cs
+++ Assets/Scripts/Yeti.cs
@@ -6,172 +6,185 @@ using UnityEngine.AI;
 
 public class Yeti : MonoBehaviour
 {
-	public GameObject player;
-	public LayerMask snowLayer;
-	public AudioClip yetiSound;
-	public AudioSource audioSource;
-	public AudioSource stompingSound;
-	
-	public AudioSource wanderResetRoar;
+    public GameObject player;
+    public LayerMask snowLayer;
+    public AudioClip yetiSound;
+    public AudioSource audioSource;
+    public AudioSource stompingSound;
 
-	public float yetiSoundDelay = 3.0f;
-	private bool canPlaySound = true;
-	
-	NavMeshAgent agent;
-	
-	private PlayerController playerController;
-	private Vector3 target; // Last position Yeti saw player
-	private bool hasReachedLastPosition = false; // Determines if Yeti has reached last position
-	private bool canSeePlayer = false; // Check if Yeti can see player
-	private float distanceCheck = 0.5f; // Distance check
-	
-	// Debug variable
-	private bool isDebug = false;
-	private int SFXEnabled;
+    public AudioSource wanderResetRoar;
 
-	public GameObject yetiNearby;
-	YetiClose yetiN;
-	//	Material yetiNMat;
+    public float yetiSoundDelay = 3.0f;
+    private bool canPlaySound = true;
+
+    NavMeshAgent agent;
+
+    private PlayerController playerController;
+    private Vector3 target; // Last position Yeti saw player
+    private bool hasReachedLastPosition = false; // Determines if Yeti has reached last position
+    private bool canSeePlayer = false; // Check if Yeti can see player
+    private float distanceCheck = 0.5f; // Distance check
+    public Inventory inventory;
+
+    // Debug variable
+    private bool isDebug = false;
+    private int SFXEnabled;
+
+    public GameObject yetiNearby;
+    YetiClose yetiN;
+    //	Material yetiNMat;
 
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		SFXEnabled = PlayerPrefs.GetInt("SFXEnabled", 1);
-		if (SFXEnabled != 1) 
-		{
-			stompingSound.mute = true;
-		}
-		
-		target = player.transform.position;
-		agent = GetComponent<NavMeshAgent>();
-		agent.updateRotation = false;
-		agent.updateUpAxis = false;
-		
-		playerController = player.GetComponent<PlayerController>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (inventory == null)
+        {
+            Debug.LogError("Inventory reference not set");
+        }
 
-		// Ensure AudioSource component is assigned
-		if (audioSource == null)
-		{
-			audioSource = gameObject.AddComponent<AudioSource>();
-		}
+        SFXEnabled = PlayerPrefs.GetInt("SFXEnabled", 1);
+        if (SFXEnabled != 1)
+        {
+            stompingSound.mute = true;
+        }
 
-		yetiNearby = GameObject.Find("YetiNearbyMaterial");
-		yetiN = yetiNearby.GetComponent<YetiClose>();
-	}
+        target = player.transform.position;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
 
-	// Update is called once per frame
-	void Update()
-	{
-		// Check if Yeti can see player
-		CheckForPlayer();
-		
-		if (Vector2.Distance(transform.position, target) <= distanceCheck) 
-		{
-			hasReachedLastPosition = true;
-		}
-		
-		if (hasReachedLastPosition == true && !canSeePlayer) 
-		{
-			target = GetWanderPoint();
-		}
-		
-		hasReachedLastPosition = false;
-		agent.SetDestination(target);
+        playerController = player.GetComponent<PlayerController>();
 
-		// Play the Yeti sound with delay
-		if (canSeePlayer && canPlaySound && yetiSound != null && audioSource != null && SFXEnabled == 1)
-		{
-			StartCoroutine(PlaySoundWithDelay(yetiSoundDelay));
-		}
-	}
-	
-	void CheckForPlayer() 
-	{
-		// Cast ray towards player
-		RaycastHit2D raycastHit2D = Physics2D.Linecast(transform.position, player.transform.position, snowLayer);
-		
-		// If debug, draw line
-		if (isDebug) 
-		{
-			Debug.DrawLine(transform.position, player.transform.position, Color.red);
-		}
+        // Ensure AudioSource component is assigned
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
 
-		float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-		// Check for collision with snow
-		if (raycastHit2D.collider != null)
-		{
-			canSeePlayer = false;
-			float normalizedDistance = Mathf.Clamp01(distanceToPlayer / 5f);
-			float yetiAway = Mathf.Lerp(yetiN.minFloatValue, yetiN.maxFloatValue, normalizedDistance + 0.1f);
-			yetiN.material.SetFloat("_VignettePower", yetiAway);
-			//Debug.Log("Yeti Away:" + yetiAway);
-			agent.speed = 5.5f;
-			// Collision with snow detected
-			if (isDebug) 
-			{
-				Debug.Log("Collision with snow detected!");
-			}
-		}
-		else
-		{
-			// No collision with snow, update player's last position
-			canSeePlayer = true;
-			agent.speed = 7.5f;
-			float normalizedDistance = Mathf.Clamp01(distanceToPlayer / 5f);
-			float yetiClose = Mathf.Lerp(yetiN.minFloatValue, yetiN.maxFloatValue, normalizedDistance - 0.1f);
-			//Debug.Log("Yeti Close:" + yetiClose);
-			yetiN.material.SetFloat("_VignettePower", yetiClose);
-			target = playerController.movePoint.transform.position;
+        yetiNearby = GameObject.Find("YetiNearbyMaterial");
+        yetiN = yetiNearby.GetComponent<YetiClose>();
+    }
 
-		}
-	}
-	
-	// Yeti will wander around the carved path until it sees the player again
-	Vector3 GetWanderPoint() 
-	{
-		// Get random point from wander points
-		List<Vector3> wanderPoints = playerController.wanderPoints;
-		
-		// Instantiate wander point to player movePoint (this will be used if list is empty to prevent Yeti from standing still)
-		Vector3 result = playerController.movePoint.transform.position;
-		
-		// Check that wanderPoints is not empty
-		if (wanderPoints.Count > 0) 
-		{
-			int randIndex = Random.Range(0, wanderPoints.Count-1);
-			
-			// Get result and delete from list
-			result = wanderPoints[randIndex];
-			wanderPoints.Remove(result);
-			playerController.wanderPoints = wanderPoints;
-		}
-		
-		return result;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if Yeti can see player
+        CheckForPlayer();
 
-	IEnumerator PlaySoundWithDelay(float delay)
-	{
-		canPlaySound = false;
-		yield return new WaitForSeconds(delay);
-		audioSource.clip = yetiSound;
-		audioSource.Play();
-		yield return new WaitForSeconds(audioSource.clip.length);
-		canPlaySound = true;
-	}
+        if (Vector2.Distance(transform.position, target) <= distanceCheck)
+        {
+            hasReachedLastPosition = true;
+        }
 
-	// Roar when player crosses a wander reset zone (yeti sound 3)
-	public void WanderResetRoar() 
-	{
-		// Set target back to player
-		if (gameObject.activeSelf) 
-		{
-			agent.SetDestination(player.transform.position);
-			
-			if (SFXEnabled == 1) 
-			{
-				wanderResetRoar.Play();
-			}
-		}
-	}
+        if (hasReachedLastPosition == true && !canSeePlayer)
+        {
+            target = GetWanderPoint();
+        }
+
+        hasReachedLastPosition = false;
+        agent.SetDestination(target);
+
+        // Play the Yeti sound with delay
+        if (canSeePlayer && canPlaySound && yetiSound != null && audioSource != null && SFXEnabled == 1)
+        {
+            StartCoroutine(PlaySoundWithDelay(yetiSoundDelay));
+        }
+    }
+    public void AdjustSpeed(float newSpeed)
+    {
+        agent.speed = newSpeed;
+    }
+    void CheckForPlayer()
+    {
+        // Cast ray towards player
+        RaycastHit2D raycastHit2D = Physics2D.Linecast(transform.position, player.transform.position, snowLayer);
+
+        // If debug, draw line
+        if (isDebug)
+        {
+            Debug.DrawLine(transform.position, player.transform.position, Color.red);
+        }
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        // Check for collision with snow
+        if (!inventory.isPowerUpActive || inventory.itemPower != Inventory.ItemPower.YetiSlowDown)
+        {
+
+            if (raycastHit2D.collider != null)
+            {
+                canSeePlayer = false;
+                float normalizedDistance = Mathf.Clamp01(distanceToPlayer / 5f);
+                float yetiAway = Mathf.Lerp(yetiN.minFloatValue, yetiN.maxFloatValue, normalizedDistance + 0.1f);
+                yetiN.material.SetFloat("_VignettePower", yetiAway);
+                //Debug.Log("Yeti Away:" + yetiAway);
+                agent.speed = 5.5f;
+                // Collision with snow detected
+                if (isDebug)
+                {
+                    Debug.Log("Collision with snow detected!");
+                }
+            }
+            else
+            {
+                // No collision with snow, update player's last position
+                canSeePlayer = true;
+                agent.speed = 7.5f;
+                float normalizedDistance = Mathf.Clamp01(distanceToPlayer / 5f);
+                float yetiClose = Mathf.Lerp(yetiN.minFloatValue, yetiN.maxFloatValue, normalizedDistance - 0.1f);
+                //Debug.Log("Yeti Close:" + yetiClose);
+                yetiN.material.SetFloat("_VignettePower", yetiClose);
+                target = playerController.movePoint.transform.position;
+
+            }
+        }
+    }
+
+    // Yeti will wander around the carved path until it sees the player again
+    Vector3 GetWanderPoint()
+    {
+        // Get random point from wander points
+        List<Vector3> wanderPoints = playerController.wanderPoints;
+
+        // Instantiate wander point to player movePoint (this will be used if list is empty to prevent Yeti from standing still)
+        Vector3 result = playerController.movePoint.transform.position;
+
+        // Check that wanderPoints is not empty
+        if (wanderPoints.Count > 0)
+        {
+            int randIndex = Random.Range(0, wanderPoints.Count - 1);
+
+            // Get result and delete from list
+            result = wanderPoints[randIndex];
+            wanderPoints.Remove(result);
+            playerController.wanderPoints = wanderPoints;
+        }
+
+        return result;
+    }
+
+    IEnumerator PlaySoundWithDelay(float delay)
+    {
+        canPlaySound = false;
+        yield return new WaitForSeconds(delay);
+        audioSource.clip = yetiSound;
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+        canPlaySound = true;
+    }
+
+    // Roar when player crosses a wander reset zone (yeti sound 3)
+    public void WanderResetRoar()
+    {
+        // Set target back to player
+        if (gameObject.activeSelf)
+        {
+            agent.SetDestination(player.transform.position);
+
+            if (SFXEnabled == 1)
+            {
+                wanderResetRoar.Play();
+            }
+        }
+    }
 }
